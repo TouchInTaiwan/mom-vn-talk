3b364af56a4176e539c649486cce9b3f7ffcbf90
WilliamWu <williamwu@WilliamWudeMacBook-Air.local>
2026-04-02 20:02:45 +0800

    在sw.js新增activate段落，強制新版的service worker接管；跟sw.js?v=' + Date.now()

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
+// sw.js
 self.addEventListener('install', (e) => {
   // 強制讓新的 Service Worker 安裝完後立即進入 active 狀態
   self.skipWaiting(); 
   
   e.waitUntil(
-    caches.open('mom-app-v1.3').then((cache) => cache.addAll([
+    // 每次更新代碼，建議手動升級這個版本號 (例如 v1.4)
+    caches.open('mom-app-v1.4').then((cache) => cache.addAll([
       './',
       './index.html'
     ]))
   );
 });
 
-// 當新的 Service Worker 啟用時，立刻接管所有頁面
+// 當新的 Service Worker 啟用時，立刻接管所有頁面控制權
 self.addEventListener('activate', (e) => {
   e.waitUntil(clients.claim()); 
 });
