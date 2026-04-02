d0af67f9b017f30937a11d09fe13c11bf4fa248d
TouchInTaiwan <118898866+TouchInTaiwan@users.noreply.github.com>
2026-04-02 17:35:49 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('mom-app-v1').then((cache) => cache.addAll([
+    caches.open('mom-app-v1.1').then((cache) => cache.addAll([
       './',
       './index.html'
     ]))
