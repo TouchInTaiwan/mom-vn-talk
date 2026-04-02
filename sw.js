498dbdfc26b489816de035e906a1368cbd0218e6
WilliamWu <williamwu@WilliamWudeMacBook-Air.local>
2026-04-02 19:39:07 +0800

    加入 skipWaiting 機制。這會強制新版的 Service Worker 在安裝完後立刻接管。

--- sw.js
+++ sw.js
@@ -1,10 +1,18 @@
 self.addEventListener('install', (e) => {
+  // 強制讓新的 Service Worker 安裝完後立即進入 active 狀態
+  self.skipWaiting(); 
+  
   e.waitUntil(
-    caches.open('mom-app-v1.2').then((cache) => cache.addAll([
+    caches.open('mom-app-v1.3').then((cache) => cache.addAll([
       './',
       './index.html'
     ]))
   );
+});
+
+// 當新的 Service Worker 啟用時，立刻接管所有頁面
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim()); 
 });
 
 self.addEventListener('fetch', (e) => {
